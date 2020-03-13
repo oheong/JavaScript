aa28a4230a2bae1e2821517d05c53078abf1d590
OHEONG <enffl9568@naver.com>
2020-03-14 01:56:36 +0900

    프론트엔드 어려어ಥ_ಥ

---
+++ webTest.java
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Insert title here</title>
+		<link href="https://fonts.googleapis.com/css?family=Waiting+for+the+Sunrise&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Hi+Melody&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Noto+Serif+KR&display=swap" rel="stylesheet">
+		<style type="text/css">
+		#container{
+			width : 100%;
+			background-color : white;
+			border : 2px solid red;
+			margin : 20px;
+			padding : 20px;
+			text-align : center;
+		}
+		h1{/*메인*/
+			text-align : center;
+			font-family: 'Waiting for the Sunrise', cursive;
+			color : white;
+			font-size : 51px;
+		}
+		h2{ /*제목*/
+			text-align : left;
+			font-family: 'Hi Melody', cursive;
+			font-size : 30px;
+		}
+		h3{/*일반*/
+			font-family: 'Noto Serif KR', serif;
+			text-align : center;
+			font-size : 16px;
+		}
+		h4{/*표*/
+			text-align : center;
+			color : gray;
+			font-size : 16px;
+		}
+		header{
+			background-color: #08088A;
+			border : 2px solid white;
+			margin-bottom: 20px;
+			padding : 20px;
+		}
+		section{ /* 본 섹션 */
+			width : 60%;
+			background-color : white;
+			border : 3px solid #5882FA;
+			margin-bottom: 20px;
+			padding : 20px;
+			float : left;
+		}
+		aside{ /* 사이드 바 */
+			width : 30%;
+			background-color : #0174DF;
+			border : 1px solid #0174DF;
+			margin-bottom : 20px;
+			padding : 20px;
+			float : right;
+		}
+		footer{
+			padding : 20px;
+			border : 1px solid #CED8F6;
+			clear : both;
+			background-color : #CED8F6;
+		}
+		table{
+			border-collapse : collapse;
+		}
+		td, th{
+			border-bottom : 1px solid gray;
+			padding : 10px;
+			text-align : center;
+		}
+		thead tr{
+			background-color : #0d47a1;
+			color : white;
+		}
+ 		tbody tr:nth-child(2n) {
+    		background-color: #bbdefb;
+  		}
+	  	tbody tr:nth-child(2n+1) {
+	   		background-color: #e3f2fd;
+	  	}
+		</style>
+	</head>
+	<body>
+		<div id = "container">
+			<header>
+				<h1>ONLINE Ss__A_fy</h1>
+				<h4>메뉴 1</h4>
+			</header>
+			<section>
+				<article>
+					<h2>기사 1</h2>
+					<h3>재산권의 행사는 공공복리에 적합하도록 하여아 한다.
+					정부는 회계연도마다 예산안을 편성하여 회계연도 개시 90일전까지 국회에 제출하고,
+					국회는 회계연도 개시 30일전까지 이를 의결하여야한다.</br></br>
+					대통령의 임기가 만료되는 때에는 임기만료 70일 내지 40일전에 후임자를 선거한다.
+					제2항과 제3항의 처분에 대하여는 법원에 제소할 수 었다.
+					언론,출한에 대한 허가나 검열과 집회,결사에 대한 허가는 인정되지 아니한다.</h3>
+				</article>
+				
+				<article>
+					<h2>기사 2</h2>
+					<table>
+						<thead>
+							<tr><td>번호</td><td>제목</td><td>작성자</td></tr>
+						</thead>
+						<tbody>
+							<tr><td>1</td><td width=450>이 헌법 시행</td><td>강감찬</td></tr>
+							<tr><td>2</td><td width=450>대한민국은</td><td>장영실</td></tr>
+							<tr><td>3</td><td width=450>정당은</td><td>안중근</td></tr>
+							<tr><td>4</td><td width=450>공공필요에</td><td>김두한</td></tr>
+							<tr><td>5</td><td width=450>국회의원은</td><td>계백</td></tr>
+							<tr><td>6</td><td width=450>정기의회</td><td>최무선</td></tr>
+						</tbody>
+					</table>
+				</article>
+			
+			</section>
+			<aside>
+				<h2>SIDEBAR</h2>
+				<h3>모든 국민은 근로의 의무를 진다. 국가는 근로의 의무의내용과 조건을 민주주의원칙에 따라 법률로 정한다.</h3>
+			</aside>
+			<footer>
+				<h2>Footer</h2>
+				<h3>이 헌법시행 당시의 법령과 조약은 이 헌법에 위배되지 아니하는 한 그 효력을 지속한다.</h3>
+			</footer>
+		</div>
+	</body>
+</html>
